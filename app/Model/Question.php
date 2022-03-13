51d6e52f9c2b096e13549d8412ec46e857f3c92c
Good Shepherd <a111@111deMacBook-Pro.local>
2022-03-13 21:30:50 +0200

    Add relationship for Question, Replay, User Classes

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Model;
-
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replays()
+    {
+        return $this->hasMany(Replay::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
