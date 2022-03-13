4fec712044df4ccba942ce1351dd2b9dd1fa322c
Good Shepherd <a111@111deMacBook-Pro.local>
2022-03-13 22:32:24 +0200

    seed @db factory

--- database/factories/ReplayFactory.php
+++ database/factories/ReplayFactory.php
@@ -3,10 +3,18 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model\Replay;
+use App\Model\Question;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Replay::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function(){
+            return Question::all()->random();
+        },
+        'user_id' => function(){
+            return \App\User::all()->random();
+        },
     ];
 });
