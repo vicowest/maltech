decf022f5e7d5763b3ac5e93c937d98d1bad2ca6
Good Shepherd <a111@111deMacBook-Pro.local>
2022-04-05 19:56:14 +0200

    CRUD for Question along with Question Resource Transformation

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Model\Question;
 use Illuminate\Http\Request;
+use App\Http\Resources\QuestionResource;
 
 class QuestionController extends Controller
 {
@@ -14,7 +15,7 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
+        return QuestionResource::collection(Question::latest()->get());
     }
 
     /**
@@ -35,7 +36,9 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Question::create($request->all());
+        //auth()->user()->question::create($request->all());
+        return response('created', Response::HTTP_CREATED);
     }
 
     /**
@@ -46,7 +49,7 @@ class QuestionController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        return new QuestionResource($question);
     }
 
     /**
@@ -80,6 +83,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return response('Deleted',204);
     }
 }
--- app/Model/Question.php
+++ app/Model/Question.php
@@ -20,4 +20,18 @@ class Question extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    // to show the sluq on any question instead of id 
+    //the should be getRouteKeyName
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected $guarded = [];
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
+    }
 }
